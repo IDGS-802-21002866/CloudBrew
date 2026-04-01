c186c0019e2f0ad0c3ee7edd9c13a6b08d8763f3
manue <manuelmhernandez37@gmail.com>
2026-04-01 14:03:48 -0600

    setup_archivos_01-04-26

--- app/__init__.py
+++ app/__init__.py
@@ -19,6 +19,7 @@ def create_app():
     import app.modules
     from app.modules.auth import bp as auth_bp
     from app.modules.main import bp as main_bp
+    from app.modules.proveedores import bp as proveedores_bp
 
     app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
@@ -35,6 +36,8 @@ def create_app():
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
+    app.register_blueprint(proveedores_bp)
+
 
     with app.app_context():
         db.create_all()
