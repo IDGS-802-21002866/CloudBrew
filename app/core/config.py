8c359d3eabbcdf61b65879433642353391009aab
migueldr12 <253625159+IDGS-802-21002866@users.noreply.github.com>
2026-04-15 13:31:39 -0600

    Fix: configuracion de produccion

--- app/core/config.py
+++ app/core/config.py
@@ -51,8 +51,9 @@ class ProductionConfig(Config):
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
-    MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
-    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", "True") == "True"
+    MAIL_PORT = int(os.environ.get("MAIL_PORT", 465))
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
